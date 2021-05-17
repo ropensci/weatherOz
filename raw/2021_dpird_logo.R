2c532a2d19e69d9e28bcaf50bcb6c493077c3ec5
Anna Hepworth <anna.hepworth@agric.wa.gov.au>
2021-05-17 17:13:03 +0800

    add data object for dpird logo and code to generate it.

--- raw/2021_dpird_logo.R
+++ raw/2021_dpird_logo.R
@@ -1,17 +1,21 @@
 # code to read in the dpird logo (which should have a copy in this folder)
 # and then create a data object for the package.
 
+# the 'read.logo' function has been copied 'as is' from the SSF package
+# and used to read in the current dpird logo; it is not currently being included
+# as a logo in the package, so the documentation has been commented out.
+
 # ---- Logo ----
-#' Read logo from file
-#'
-#' Reads PNG format logo and returns as class 'raster'.
-#'
-#' @param filename Name of PNG file. If this is not provided, it is assumed that
-#'   the desired file is "DPIRD_black.png", and that this is in the working
-#'   directory.
-#'
-#' @keywords hplot
-#' @export
+# Read logo from file
+#
+# Reads PNG format logo and returns as class 'raster'.
+#
+# @param filename Name of PNG file. If this is not provided, it is assumed that
+  # the desired file is "DPIRD_black.png", and that this is in the working
+  # directory.
+  #
+# @keywords hplot
+# @export
 
 read.logo <- function(
   filename = "DPIRD_black.png") {
@@ -21,3 +25,11 @@ read.logo <- function(
 
 }
 
+dpird.logo <- read.logo(
+  filename = "raw/DPIRD_black.png"
+  )
+
+# create the file as data file
+usethis::use_data(
+  dpird.logo,
+  overwrite = TRUE)
