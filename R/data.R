b81ed60f000decafca6ac6d293b503d772cb1cea
Anna Hepworth <anna.hepworth@agric.wa.gov.au>
2021-05-17 17:16:41 +0800

    add documentation for dpird.logo data object.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' DPIRD logo
+#'
+#' Black and white logo for the Department of Primary Industries and Regional Development.
+#' Current as of May 2021.
+#'
+#' @name dpird.logo
+#' @docType data
+#' @keywords datasets
+#'
+#' @format raster
+#' @source version in use in the SSF copied; original source forgotten
+"dpird.logo"
