e9760911899d33e9cf286982d03c77a9673b0aea
Anna Hepworth <anna.hepworth@agric.wa.gov.au>
2021-05-19 14:37:29 +0800

    add weather_plot function based on new.ssf.plot from SSF package

--- R/map_functions.R
+++ R/map_functions.R
@@ -3,6 +3,9 @@
 # The 'map.ssf' function from that package is the 'map_weather' function; the
 # masks for the South West Land Division (SWLD) are now used in map_weather_swld
 # rather than being the defaults for the more general function.
+#
+# Note that the 'read logo' function is not included; the DPIRD logo is included
+# in this package as a data object.
 
 # ---- Legend ----
 #' Add legend
@@ -150,4 +153,80 @@ make_col_set <- function(col,
     a$label <- as.character(key)
   }
   return(a)
+}
+
+# -- New Plot ----
+#' Opens a plot device
+#'
+#' Opens the specified type of device to fit a plot of the desired aspect ratio
+#' and size. different plot devices require different values to get the same
+#' sized plot. This has been specifically set up for the SWLD of WA, as
+#' per the maps from the SSF. It is included in this package as an interim
+#' function and is likely to be deprecated and removed in later versions.
+#'
+#' @param type plot type (0 = separate window, 1 = jpg, 4 = png). Default = 0.
+#'   Note: png files will give a clearer plot and a smaller file for the same
+#'   width/height parameters.
+#' @param width plot width. These are converted internally; Finished measurement
+#'   in inches is ~ width/10. Saved image files are slightly different from
+#'   those opened in a new window. This is not a change to be dealt with now.
+#' @param height plot height (units as per width)
+#' @param name (something something file name; will have _probs and then file
+#'   type appended) Defaults to NULL to allow for opening an unnamed window.
+#' @param path directory in which the file will be saved for types 1 through 4.
+#'   Default is NULL.
+#' @param background background colour. Will only be relevant if type is 4.
+#'
+#' @export
+
+weather_plot <- function(type = 0,
+                         width,
+                         height,
+                         name = NULL,
+                         path = NULL,
+                         background = "white") {
+
+  # assume that if path is given as blank it means current folder;
+  # not required if plot type is 0
+  if (type != 0) {
+    if (path == "" | is.null(path)) {
+      path <- "."
+    }
+  }
+
+  # ** pointsize; gamma; xpos; ypos set for option 0 as per help file for x11;
+  # these may need changing
+  switch(as.character(type),
+    "0" = {
+      grDevices::x11(
+        width = width / 10,
+        height = height / 10,
+        pointsize = 12,
+        bg = background,
+        gamma = 1,
+        xpos = -25,
+        ypos = 0,
+        title = ""
+      )
+    },
+    "1" = {
+      save.file <- paste0(name, ".jpg")
+      grDevices::jpeg(
+        filename = file.path(path, save.file),
+        width = width * 3,
+        height = height * 3,
+        bg = background
+      )
+    },
+    "4" = {
+      save.file <- paste0(name, ".png")
+      grDevices::png(
+        filename = file.path(path, save.file),
+        width = width * 3,
+        height = height * 3,
+        bg = background
+      )
+    },
+    message("Type is not supported")
+  )
 }
