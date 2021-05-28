ca42b6c3c8938f39a794b0c9f47e9c04b9612110
Adam Sparks <adamhsparks@gmail.com>
2021-05-28 12:18:53 +0800

    Clean up documentation

--- R/get_weather_list.R
+++ R/get_weather_list.R
@@ -37,7 +37,7 @@
 #'  * rainfall (rain),
 #'  * maximum temperature (max_temp),
 #'  * minimum temperature (min_temp) and
-#'  * \acronym{DPIRD} science \acronym{API}'s station code.
+#'  * \acronym{DPIRD}'s Science 2.0 \acronym{API} station code.
 #'
 #' @author Adam Sparks, adam.sparks@@dpird.wa.gov.au
 #' @export
