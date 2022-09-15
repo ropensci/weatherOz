ba7097f620ddb4a580b9b4c65d1f2780fa27bd43
Adam Sparks <adamhsparks@gmail.com>
2022-09-15 14:08:41 +0800

    Add check for user-entered dates

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -8,8 +8,40 @@
 #' @example x %notin% y
 #' @keywords internal
 #' @return A vector of Boolean values the same length as `x` and `y`
-#' @author Adam Sparks, adam.sparks@@dpird.wa.gov.au
+#' @author Adam Sparks, \email{adam.sparks@@dpird.wa.gov.au}
 #' @noRd
 `%notin%` <- function(x, y) {
   match(x, y, nomatch = 0L) == 0L
+}
+
+#' Check user-provided dates for validity
+#'
+#' @param x User entered date value
+#' @return Validated date string as a `POSIXct` object.
+#' @note This was taken from \CRANpkg{nasapower}.
+#' @example .check_date(x)
+#' @author Adam Sparks, \email{adam.sparks@@dpird.wa.gov.au}
+#' @keywords internal
+#' @noRd
+.check_date <- function(x) {
+  tryCatch(
+    x <- lubridate::parse_date_time(x,
+                                    c(
+                                      "Ymd",
+                                      "dmY",
+                                      "mdY",
+                                      "BdY",
+                                      "Bdy",
+                                      "bdY",
+                                      "bdy"
+                                    )),
+    warning = function(c) {
+      stop(call. = FALSE,
+           "\n",
+           x,
+           " is not in a valid date format. Please enter a valid date format.",
+           "\n")
+    }
+  )
+  return(x)
 }
