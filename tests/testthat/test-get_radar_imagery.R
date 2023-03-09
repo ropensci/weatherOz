a3d50d2e234ed7433e1000b23b6066389caacdec
Adam Sparks <adam.sparks@dpird.wa.gov.au>
2023-03-09 08:58:32 +0800

    update tests for get_radar_imagery

--- tests/testthat/test-get_radar_imagery.R
+++ tests/testthat/test-get_radar_imagery.R
@@ -12,12 +12,12 @@ test_that("get_available_radar functions properly", {
   expect_gt(nrow(x), 0)
 
   xx <- get_available_radar()
-  expect_is(xx, "data.frame")
+  expect_s3_class(xx, "data.frame")
   expect_gt(nrow(xx), 0)
   expect_equal(xx, get_available_radar(radar_id = "all"))
 
   xxx <- get_available_radar(radar_id = 3)
-  expect_is(xxx, "data.frame")
+  expect_s3_class(xxx, "data.frame")
   expect_gt(nrow(xxx), 0)
   expect_lt(nrow(xxx), 5)
   expect_equal(unique(xxx$Radar_id), 3)
@@ -34,7 +34,7 @@ test_that("Error handling works", {
 test_that("get_radar_imagery functions properly", {
   skip_on_cran()
   y <- get_radar_imagery(product_id = "IDR022")
-  expect_is(y, "magick-image")
+  expect_type(y, "externalptr")
 
   yy <- get_radar_imagery(product_id = "IDR022", download_only = TRUE)
   expect_null(yy)
