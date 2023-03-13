7e4e8245065606fec696486cfedaa11410f1ae25
Adam Sparks <adam.sparks@dpird.wa.gov.au>
2023-03-13 12:26:19 +0800

    Redoc with new name

---
+++ R/weatherOz-package.R
@@ -0,0 +1,17 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom data.table :=
+#' @importFrom data.table .BY
+#' @importFrom data.table .EACHI
+#' @importFrom data.table .GRP
+#' @importFrom data.table .I
+#' @importFrom data.table .N
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .SD
+#' @importFrom data.table data.table
+## usethis namespace: end
+NULL
