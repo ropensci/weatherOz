dd54d92d8ae8e660b7e6c98e344b017a254292e0
Adam H. Sparks <adamhsparks@icloud.com>
2023-04-12 19:12:33 +0800

    Add functionality to get BOM stations in SILO by distance

--- R/get_nearest_silo_stations.R
+++ R/get_nearest_silo_stations.R
@@ -3,19 +3,27 @@
 #' Searches for stations available in the SILO database and returnes values
 #' sorted from nearest to farthest.
 #'
-#' @param distance_km A `numeric` value for the distance in kilometres in which to
-#'  search for nearby stations from a given geographic location or known
-#'  `station_id`.
+#' @param distance_km A `numeric` value for the distance in kilometres in which
+#'  to search for nearby stations from a given geographic location or known
+#'  `station_id`. Cannot be used when `longitude` and `latitude` are provided.
 #' @param longitude A `numeric` value for longitude expressed as decimal degrees
-#'  (DD) (WGS84). Required if `latitude` is used.
+#'  (DD) (WGS84). Required if `latitude` is used. Cannot be used if `station_id`
+#'  is provided.
 #' @param latitude A `numeric` value for latitude expressed as decimal degrees
-#'   (DD) (WGS84). Required if `longitude` is used.
+#'   (DD) (WGS84). Required if `longitude` is used. Cannot be used if
+#'   `station_id` is provided.
 #' @param station_id A `character` value for a known station from which the
 #' nearby stations are to be identified.
 #'
 #' @examplesIf interactive()
-#' find_nearby_silo_stations <- function(radius = 500,
-#'                                       station_id = "015526")
+#' # find stations within 50 km of Finke Post Office, NT
+#' find_nearby_silo_stations(distance_km = 50,
+#'                          station_id = "015526")
+#'
+#' # find stations within 500 km of Esperance, WA
+#' find_nearby_silo_stations(distance_km = 50,
+#'                           longitude = 121.8913,
+#'                           latitude = -33.8614)
 #'
 #' @return A `data.table` of available stations and associated metadata.
 #'
@@ -24,39 +32,48 @@ find_nearby_silo_stations <- function(distance_km,
                                       longitude = NULL,
                                       latitude = NULL,
                                       station_id = NULL) {
-
+  if (!is.null(station_id) &&
+      !is.null(longitude) && !is.null(latitude)) {
+    stop(
+      call. = FALSE,
+      "You have provided `station_id` along with `longitude` and \n",
+      "`latitude` values. Please provide only one, either `station_id` \n",
+      "or a pairing of `longitude` and `latitude` values."
+    )
+  }
   if (!is.null(longitude) || !is.null(latitude)) {
     stopifnot("Provide both `latitude` and `longitude` values" =
                 length(latitude) == length(longitude))
     .check_lonlat(longitude = longitude, latitude = latitude)
   }
 
-  x <- .get_silo_stations()
+  x <-
+    .get_silo_stations(.station_id = station_id, .distance_km = distance_km)
 
-  # if lon/lat are requested ----
-  if (is.null(station_id) &&
-      !is.null(latitude) & !is.null(longitude)) {
+  if (!is.null(station_id)) {
+    return(x)
+  } else if (is.null(station_id)) {
     x[, "distance" := .haversine_distance(latitude,
                                           longitude,
                                           lat,
                                           lon)] |>
       data.table::setorderv("distance")
-    x[distance %in%
-        x[(distance <= distance_km)]$distance]
-    return(x[])
-  } else {
-    .station_id <- station_id
-    x <- x[station_id %in% .station_id]
-    x[, "distance" := .haversine_distance(latitude,
-                                          longitude,
-                                          lat,
-                                          lon)] |>
-      data.table::setorderv("distance")
-    x[distance %in%
-        x[(distance <= distance_km)]$distance]
+
+    x[, distance_km := NULL]
+
+    return(x[distance %in%
+               x[(distance <= distance_km)]$distance])
   }
 }
+
 #' Create a data.table object of all stations available in SILO
+#'
+#' @param .station_id A string identifying a BOM station in SILO which should
+#' be used as the centre point to determine stations that fall within
+#'  `.distance_km`
+#' @param .distance_km A `numeric` value for the distance in kilometres in which
+#'  to search for nearby stations from a given geographic location or known
+#'  `.station_id`.
 #'
 #' Uses SILO's cgi-bin web interface to query and download a text file of
 #' BOM weather stations in SILO within 2500 km of Finke, NT (near the Lambert
@@ -64,20 +81,39 @@ find_nearby_silo_stations <- function(distance_km,
 #' Finke Post Office.
 #'
 #' @noRd
-.get_silo_stations <- function() {
-  curl::curl_download(url = "https://www.longpaddock.qld.gov.au/cgi-bin/silo/PatchedPointDataset.php?format=near&station=015526&radius=2500",
-                      destfile = file.path(tempdir(), "stations.txt"))
-  r <- data.table::fread(file.path(tempdir(), "stations.txt"))[, -7]
-  data.table::setnames(r,
-                       names(r),
-                       c(
-                         "station_id",
-                         "station_name",
-                         "lat",
-                         "lon",
-                         "state",
-                         "elevation"
-                       ))
+.get_silo_stations <- function(.station_id, .distance_km) {
+  base_url <-
+    "https://www.longpaddock.qld.gov.au/cgi-bin/silo/PatchedPointDataset.php?format="
+
+  # if `.station_id` is not provided, fetch all stations in AU. Otherwise, use
+  # the cgi-bin interface to find stations within `.distance_km` of the given
+  # `.station_id`
+  if (is.null(.station_id)) {
+    r <- data.table::fread(paste0(base_url,
+                                  "near&station=015526&radius=2500"))
+  } else {
+    r <- data.table::fread(paste0(
+      base_url,
+      "near&station=",
+      .station_id,
+      "&radius=",
+      .distance_km
+    ))
+  }
+
+  data.table::setnames(
+    r,
+    names(r),
+    c(
+      "station_id",
+      "station_name",
+      "lat",
+      "lon",
+      "state",
+      "elevation",
+      "distance_km"
+    )
+  )
   data.table::setkey(r, "station_id")
-  return(r)
+  return(r[])
 }
