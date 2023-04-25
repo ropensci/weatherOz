be6bc4cd230b5009d3216e995823cdcd31a9ce7c
Adam H. Sparks <adamhsparks@icloud.com>
2023-04-25 13:12:46 +0800

    Don't export documentation for {terra} re-exported print() method

--- R/get_satellite_imagery.R
+++ R/get_satellite_imagery.R
@@ -377,6 +377,7 @@ get_satellite_imagery <- get_satellite <-
 #' @param x An object of class `SpatRaster`.
 #' @param y Not used.
 #' @param ... Plot parameters forwarded.
+#' @noRd
 #' @export
 methods::setMethod(
   "plot",
