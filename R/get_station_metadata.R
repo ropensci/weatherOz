040ed0a5c88c03c0b55bfce4cd166472ef0c5b4c
Adam H. Sparks <adamhsparks@icloud.com>
2023-05-03 07:56:37 +0800

    Integrate results from both APIs

--- R/get_station_metadata.R
+++ R/get_station_metadata.R
@@ -22,20 +22,42 @@
 #'  the \acronym{SILO} network; or "dpird" for stations in the \acronym{DPIRD}
 #'  network.
 #'
+#'  @note \acronym{BOM} does not report the exact date on which stations opened
+#'   or closed, only the year. Therefore the 'start' and 'end' columns will
+#'   indicate January 1 of the year that a station opened or closed for
+#'   \acronym{BOM} stations whereas stations in the \acronym{DPIRD} network
+#'   have the date to the day. For \acronym{BOM} stations that are closed
+#'   for the current year, this indicates that the station closed sometime
+#'   during the current year prior to the request being made.
+#'
 #' @examples
 #' \dontrun{
 #' get_station_metadata()
 #' }
 #' @return a `data.table` of \acronym{BOM} weather stations' metadata for
-#'  stations available from \acronym{SILO}.
+#'  stations available from \acronym{SILO} with the following columns.
+#'  \tabular{rl}{
+#'   **station_code**:\tab Unique station ID. `factor`\cr
+#'   **station_name**:\tab Unique station name. `character`\cr
+#'   **start**:\tab Date observations start. `date`\cr
+#'   **end**:\tab Date observations end. `date`\cr
+#'   **lat**:\tab Latitude in decimal degrees. `numeric`\cr
+#'   **lon**:\tab Longitude in decimal degrees. `numeric`\cr
+#'   **state**:\tab State in which the station is located. `character`\cr
+#'   **elev.m**:\tab Station elevation in metres. `numeric`\cr
+#'   **source**:\tab Organisation responsible for the data or station
+#'    maintenance. `character`\cr
+#'   **status**:\tab Station status, one of "open" or "closed". `character`\cr
+#'   **wmo**:\tab: World Meteorlogical Organisation, acronym{WMO}, number if
+#'    applicable. `numeric`\cr
+#'   }
 #'
 #' @references
 #' Station location and other metadata are sourced from the Australian Bureau of
 #' Meteorology (\acronym{BOM}) webpage, Bureau of Meteorology Site Numbers:\cr
 #' <http://www.bom.gov.au/climate/cdo/about/site-num.shtml> and
-#' <http://www.bom.gov.au/climate/data/lists_by_element/stations.txt>
-#'
-#' @family SILO
+#' <http://www.bom.gov.au/climate/data/lists_by_element/stations.txt> and
+#' \acronym{DPIRD} Weather 2.0 \acronym{API}.
 #'
 #' @author Adam H. Sparks, \email{adam.sparks@@dpird.wa.gov.au}
 #' @export
@@ -44,7 +66,7 @@ get_station_metadata <-
   function(check_location = FALSE,
            api_key = NULL,
            which_api = "silo") {
-    which_api <- .check_which_api()
+    which_api <- .check_which_api(which_api)
 
     if (which_api == "silo") {
       silo <- .fetch_silo_metadata(.check_location = check_location)
@@ -55,8 +77,25 @@ get_station_metadata <-
       dpird <- .fetch_dpird_metadata(.api_key = api_key)
     }
 
-    out <- #Merge dpird and silo dfs here
-      return(out)
+    out <- data.table::rbindlist(list(silo, dpird))
+    data.table::setkey(out, "station_code")
+    data.table::setorderv(out, cols = c("state", "station_name"))
+    out[, start := data.table::fifelse(is.na(start),
+                                       as.character(lubridate::year(Sys.Date())),
+                                       start)]
+    out[, start := data.table::fifelse(nchar(start) == 4,
+                                       paste(start, "01", "01", sep = "-"),
+                                       start)]
+    out[, start := lubridate::ymd(start)]
+
+    out[, end := data.table::fifelse(is.na(end),
+                                     as.character(lubridate::year(Sys.Date())),
+                                     end)]
+    out[, end := data.table::fifelse(nchar(end) == 4,
+                                     paste(end, "01", "01", sep = "-"),
+                                     end)]
+    out[, end := lubridate::ymd(end)]
+    return(out)
   }
 
 .fetch_silo_metadata <- function(.check_location = check_location) {
@@ -83,7 +122,7 @@ get_station_metadata <-
     data.table::setDT(
       readr::read_fwf(
         file = file_in,
-        na = c("..", ".....", " "),
+        na = c("..", "...", ".....", " "),
         skip = 4,
         col_positions = readr::fwf_cols(
           "station_code" = c(1, 8),
@@ -127,6 +166,7 @@ get_station_metadata <-
   bom_stations[, dist := NULL]
   bom_stations[, source := NULL]
   bom_stations[, bar_height.m := NULL]
+  bom_stations[, source := "Bureau of Meteorology"]
 
   # if ASGS.foyer is installed, correct the state column, otherwise skip
   if (requireNamespace("ASGS.foyer", quietly = TRUE)) {
@@ -160,6 +200,23 @@ get_station_metadata <-
       bom_stations[, actual_state := NULL]
     }
   }
+
+  data.table::setcolorder(
+    bom_stations,
+    neworder = c(
+      "station_code",
+      "station_name",
+      "start",
+      "end",
+      "lat",
+      "lon",
+      "state",
+      "elev.m",
+      "source",
+      "status",
+      "wmo"
+    )
+  )
 
   silo_stations <-
     find_nearby_stations(
@@ -218,11 +275,11 @@ get_station_metadata <-
 
   response$raise_for_status()
   # create meta object
-  r <- jsonlite::fromJSON(response$parse("UTF8"))
-  r <- data.table::data.table(r$collection)
+  dpird_stations <- jsonlite::fromJSON(response$parse("UTF8"))
+  dpird_stations <- data.table::data.table(dpird_stations$collection)
 
   data.table::setnames(
-    r,
+    dpird_stations,
     old = c(
       "stationCode",
       "stationName",
@@ -230,7 +287,8 @@ get_station_metadata <-
       "longitude",
       "altitude",
       "startDate",
-      "endDate"
+      "endDate",
+      "owner"
     ),
     new = c(
       "station_code",
@@ -239,8 +297,29 @@ get_station_metadata <-
       "lon",
       "elev.m",
       "start",
-      "end"
+      "end",
+      "source"
     )
   )
-  return(r)
+
+  dpird_stations[, wmo := NA]
+  dpird_stations[, state := "WA"]
+
+  data.table::setcolorder(
+    dpird_stations,
+    neworder = c(
+      "station_code",
+      "station_name",
+      "start",
+      "end",
+      "lat",
+      "lon",
+      "state",
+      "elev.m",
+      "source",
+      "status",
+      "wmo"
+    )
+  )
+  return(dpird_stations[])
 }
