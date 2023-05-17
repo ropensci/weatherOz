abdba30ef603c022e17c79215cfbb427b5623bdf
Adam Sparks <adam.sparks@dpird.wa.gov.au>
2023-05-17 15:04:18 +0800

    Remove "period.*" from list of values that are valid to fetch. include by default

--- R/dpird_summary_values.R
+++ R/dpird_summary_values.R
@@ -1,9 +1,9 @@
 #' Summary weather data values available from the DPIRD Weather 2.0 'API'
 #'
-#' A [vector] object containing 84 items representing valid values to supply
+#' A [vector] object containing 75 items representing valid values to supply
 #'  to `get_dpird_summary()`'s `values` argument.
 #'
-#' @format A [vector] object of 84 items.
+#' @format A [vector] object of 75 items.
 #'
 #' @source <https://www.agric.wa.gov.au/weather-api-20>
 #'
