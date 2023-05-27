5814cbbf7147a8c5856ed092b5c4bf257b7cd5da
Adam H. Sparks <adamhsparks@icloud.com>
2023-05-27 10:47:44 +0800

    add tests for querying DPIRD station metadata using {vcr}

--- tests/testthat/test-get_station_metadata.R
+++ tests/testthat/test-get_station_metadata.R
@@ -1,30 +1,87 @@
 
-test_that("get_station_metadata() functions properly for SILO", {
-  skip_on_cran()
-  skip_if_offline()
-  x <-
-    get_station_metadata(which_api = "silo")
-  expect_equal(ncol(x), 11)
-  expect_s3_class(x, "data.table")
-  expect_named(
-    x,
-    c(
-      "station_code",
-      "station_name",
-      "start",
-      "end",
-      "latitude",
-      "longitude",
-      "state",
-      "elev.m",
-      "source",
-      "status",
-      "wmo"
-    )
-  )
-})
+test_that("get_station_metadata() functions properly for which_api = 'SILO'",
+          {
+            skip_if_offline()
+            x <-
+              get_station_metadata(which_api = "silo")
+            expect_equal(ncol(x), 11)
+            expect_s3_class(x, "data.table")
+            expect_named(
+              x,
+              c(
+                "station_code",
+                "station_name",
+                "start",
+                "end",
+                "latitude",
+                "longitude",
+                "state",
+                "elev.m",
+                "source",
+                "status",
+                "wmo"
+              )
+            )
+          })
 
-test_that("get_station_metadata() errors if no API key is provide for DPIRD",{
-  expect_error(get_station_metadata(which_api = "dpird"))
-  expect_error(get_station_metadata(which_api = "all"))
-})
+test_that("get_station_metata() functions properly for which_api = 'DPIRD'",
+          {
+            vcr::use_cassette("dpird_station_metadata", {
+              skip_if_offline()
+              x <-
+                get_station_metadata(which_api = "dpird",
+                                     api_key = Sys.getenv("DPIRD_API_KEY"))
+            })
+            expect_s3_class(x, "data.table")
+            expect_equal(ncol(x), 11)
+            expect_named(
+              x,
+              c(
+                "station_code",
+                "station_name",
+                "start",
+                "end",
+                "latitude",
+                "longitude",
+                "state",
+                "elev.m",
+                "source",
+                "status",
+                "wmo"
+              )
+            )
+          })
+
+test_that("get_station_metata() functions properly for which_api = 'all'",
+          {
+            vcr::use_cassette("all_station_metadata", {
+              skip_if_offline()
+              x <-
+                get_station_metadata(which_api = "all",
+                                     api_key = Sys.getenv("DPIRD_API_KEY"))
+            })
+            expect_s3_class(x, "data.table")
+            expect_equal(ncol(x), 11)
+            expect_named(
+              x,
+              c(
+                "station_code",
+                "station_name",
+                "start",
+                "end",
+                "latitude",
+                "longitude",
+                "state",
+                "elev.m",
+                "source",
+                "status",
+                "wmo"
+              )
+            )
+          })
+
+test_that("get_station_metadata() errors if no API key is provide for DPIRD",
+          {
+            expect_error(get_station_metadata(which_api = "dpird"))
+            expect_error(get_station_metadata(which_api = "all"))
+          })
