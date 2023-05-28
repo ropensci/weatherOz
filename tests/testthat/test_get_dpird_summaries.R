ceaabc2353e75ca3056da1b5ff001dddbd86c3ee
Adam H. Sparks <adamhsparks@icloud.com>
2023-05-28 17:39:21 +0800

    Add tests and cassette for daily summaries

--- tests/testthat/test_get_dpird_summaries.R
+++ tests/testthat/test_get_dpird_summaries.R
@@ -68,3 +68,40 @@ test_that("get_dpird_summaries() returns monthly values",
               )
             )
           })
+
+
+test_that("get_dpird_summaries() returns daily values",
+          {
+            vcr::use_cassette("dpird_daily_summaries", {
+              skip_if_offline()
+              x <- get_dpird_summaries(
+                station_code = "BI",
+                start_date = "20171028",
+                end_date = "20181028",
+                api_key = Sys.getenv("DPIRD_API_KEY"),
+                interval = "daily",
+                which_values = "wind"
+              )
+            })
+            expect_s3_class(x, "data.table")
+            expect_equal(ncol(x), 14)
+            expect_named(
+              x,
+              c(
+                "station_code",
+                "station_name",
+                "period.year",
+                "period.month",
+                "period.day",
+                "period.hour",
+                "period.minute",
+                "date",
+                "wind.avg.speed",
+                "wind.height",
+                "wind.max.direction.compass_point",
+                "wind.max.direction.degrees",
+                "wind.max.speed",
+                "wind.max.time"
+              )
+            )
+          })
