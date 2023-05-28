1813299960780bcbaf2549ef2f24223aa592f0e4
Adam H. Sparks <adamhsparks@icloud.com>
2023-05-28 17:37:13 +0800

    Add monthly summary tests and cassette

--- tests/testthat/test_get_dpird_summaries.R
+++ tests/testthat/test_get_dpird_summaries.R
@@ -32,3 +32,39 @@ test_that("get_dpird_summaries() returns yearly values",
               )
             )
           })
+
+test_that("get_dpird_summaries() returns monthly values",
+          {
+            vcr::use_cassette("dpird_monthly_summaries", {
+              skip_if_offline()
+              x <- get_dpird_summaries(
+                station_code = "BI",
+                start_date = "20171028",
+                end_date = "20231028",
+                api_key = Sys.getenv("DPIRD_API_KEY"),
+                interval = "monthly",
+                which_values = "wind"
+              )
+            })
+            expect_s3_class(x, "data.table")
+            expect_equal(ncol(x), 14)
+            expect_named(
+              x,
+              c(
+                "station_code",
+                "station_name",
+                "period.year",
+                "period.month",
+                "period.day",
+                "period.hour",
+                "period.minute",
+                "date",
+                "wind.avg.speed",
+                "wind.height",
+                "wind.max.direction.compass_point",
+                "wind.max.direction.degrees",
+                "wind.max.speed",
+                "wind.max.time"
+              )
+            )
+          })
