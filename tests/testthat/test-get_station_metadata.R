59a3ac84132e45d91a0a6e7d0ce46f7780c04fbf
Adam H. Sparks <adamhsparks@icloud.com>
2023-05-31 19:23:22 +0800

    update tests

--- tests/testthat/test-get_station_metadata.R
+++ tests/testthat/test-get_station_metadata.R
@@ -3,7 +3,7 @@ test_that("get_station_metadata() functions properly for which_api = 'SILO'",
           {
             skip_if_offline()
             x <-
-              get_station_metadata(which_api = "silo")
+              get_station_metadata(which_api = "silo", status = TRUE)
             expect_equal(ncol(x), 11)
             expect_s3_class(x, "data.table")
             expect_named(
@@ -30,7 +30,10 @@ test_that("get_station_metata() functions properly for which_api = 'DPIRD'",
               skip_if_offline()
               x <-
                 get_station_metadata(which_api = "dpird",
-                                     api_key = Sys.getenv("DPIRD_API_KEY"))
+                                     api_key = Sys.getenv("DPIRD_API_KEY"),
+                                     status = TRUE,
+                                     rich = FALSE
+                                     )
             })
             expect_s3_class(x, "data.table")
             expect_equal(ncol(x), 11)
