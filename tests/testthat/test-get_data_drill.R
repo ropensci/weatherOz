3bd895c7937f5d28a45e95d2535f1a2b153c9c55
Adam H. Sparks <adamhsparks@icloud.com>
2023-06-22 07:44:33 +0800

    Update tests and recreate cassettes

--- tests/testthat/test-get_data_drill.R
+++ tests/testthat/test-get_data_drill.R
@@ -1,4 +1,5 @@
 
+
 test_that("get_data_drill() user-input checks stop on invalid values", {
   # no longitude
   expect_error(
@@ -11,13 +12,11 @@ test_that("get_data_drill() user-input checks stop on invalid values", {
   )
 
   # no latitude
-  expect_error(
-    get_data_drill(
-      longitude = 150.05,
-      end_date = "20220501",
-      api_key = Sys.getenv("SILO_API_KEY")
-    )
-  )
+  expect_error(get_data_drill(
+    longitude = 150.05,
+    end_date = "20220501",
+    api_key = Sys.getenv("SILO_API_KEY")
+  ))
 
   # no start date
   expect_error(
@@ -30,13 +29,11 @@ test_that("get_data_drill() user-input checks stop on invalid values", {
   )
 
   # no api_key
-  expect_error(
-    get_data_drill(
-      latitude = -27.85,
-      longitude = 150.05,
-      start_date = "20220501"
-    )
-  )
+  expect_error(get_data_drill(
+    latitude = -27.85,
+    longitude = 150.05,
+    start_date = "20220501"
+  ))
 
   # bad values
   expect_error(
@@ -51,8 +48,8 @@ test_that("get_data_drill() user-input checks stop on invalid values", {
   )
 })
 
-test_that("get_data_drill() returns daily values", {
-  vcr::use_cassette("silo_get_data_drill_daily_values", {
+test_that("get_data_drill() returns all daily values", {
+  vcr::use_cassette("silo_get_data_drill_all_daily_values", {
     skip_if_offline()
     wd <- get_data_drill(
       latitude = -27.85,
@@ -76,40 +73,75 @@ test_that("get_data_drill() returns daily values", {
       "day",
       "daily_rain",
       "daily_rain_source",
-      "max_temp",
-      "max_temp_source",
-      "min_temp",
-      "min_temp_source",
-      "vp",
-      "vp_source",
-      "vp_deficit",
-      "vp_deficit_source",
-      "evap_pan",
-      "evap_pan_source",
-      "evap_syn",
-      "evap_syn_source",
-      "evap_comb",
-      "evap_comb_source",
-      "evap_morton_lake",
-      "evap_morton_lake_source",
-      "radiation",
-      "radiation_source",
-      "rh_tmax",
-      "rh_tmax_source",
-      "rh_tmin",
-      "rh_tmin_source",
-      "et_short_crop",
-      "et_short_crop_source",
-      "et_tall_crop",
-      "et_tall_crop_source",
       "et_morton_actual",
       "et_morton_actual_source",
       "et_morton_potential",
       "et_morton_potential_source",
       "et_morton_wet",
       "et_morton_wet_source",
+      "et_short_crop",
+      "et_short_crop_source",
+      "et_tall_crop",
+      "et_tall_crop_source",
+      "evap_comb",
+      "evap_comb_source",
+      "evap_morton_lake",
+      "evap_morton_lake_source",
+      "evap_pan",
+      "evap_pan_source",
+      "evap_syn",
+      "evap_syn_source",
+      "max_temp",
+      "max_temp_source",
+      "min_temp",
+      "min_temp_source",
       "mslp",
-      "mslp_source"
+      "mslp_source",
+      "radiation",
+      "radiation_source",
+      "rh_tmax",
+      "rh_tmax_source",
+      "rh_tmin",
+      "rh_tmin_source",
+      "vp",
+      "vp_deficit",
+      "vp_deficit_source",
+      "vp_source"
+    )
+  )
+  expect_s3_class(wd, class = "data.table")
+})
+
+test_that("get_data_drill() returns selected daily values", {
+  vcr::use_cassette("silo_get_data_drill_selected_daily_values", {
+    skip_if_offline()
+    wd <- get_data_drill(
+      latitude = -27.85,
+      longitude = 150.05,
+      which_values = c("rain", "max_temp", "min_temp"),
+      start_date = "2021-06-01",
+      end_date = "2021-07-01",
+      api_key = Sys.getenv("SILO_API_KEY")
+    )
+  })
+  expect_equal(nrow(wd), 31)
+  expect_length(wd, 13)
+  expect_named(
+    wd,
+    c(
+      "longitude",
+      "latitude",
+      "elev_m",
+      "date",
+      "year",
+      "month",
+      "day",
+      "daily_rain",
+      "daily_rain_source",
+      "max_temp",
+      "max_temp_source",
+      "min_temp",
+      "min_temp_source"
     )
   )
   expect_s3_class(wd, class = "data.table")
--- tests/testthat/test-get_patched_point.R
+++ tests/testthat/test-get_patched_point.R
@@ -64,41 +64,83 @@ test_that("get_patched_point() returns daily values", {
       "day",
       "daily_rain",
       "daily_rain_source",
-      "max_temp",
-      "max_temp_source",
-      "min_temp",
-      "min_temp_source",
-      "vp",
-      "vp_source",
-      "vp_deficit",
-      "vp_deficit_source",
-      "evap_pan",
-      "evap_pan_source",
-      "evap_syn",
-      "evap_syn_source",
-      "evap_comb",
-      "evap_comb_source",
-      "evap_morton_lake",
-      "evap_morton_lake_source",
-      "radiation",
-      "radiation_source",
-      "rh_tmax",
-      "rh_tmax_source",
-      "rh_tmin",
-      "rh_tmin_source",
-      "et_short_crop",
-      "et_short_crop_source",
-      "et_tall_crop",
-      "et_tall_crop_source",
       "et_morton_actual",
       "et_morton_actual_source",
       "et_morton_potential",
       "et_morton_potential_source",
       "et_morton_wet",
       "et_morton_wet_source",
+      "et_short_crop",
+      "et_short_crop_source",
+      "et_tall_crop",
+      "et_tall_crop_source",
+      "evap_comb",
+      "evap_comb_source",
+      "evap_morton_lake",
+      "evap_morton_lake_source",
+      "evap_pan",
+      "evap_pan_source",
+      "evap_syn",
+      "evap_syn_source",
+      "max_temp",
+      "max_temp_source",
+      "min_temp",
+      "min_temp_source",
       "mslp",
-      "mslp_source"
+      "mslp_source",
+      "radiation",
+      "radiation_source",
+      "rh_tmax",
+      "rh_tmax_source",
+      "rh_tmin",
+      "rh_tmin_source",
+      "vp",
+      "vp_deficit",
+      "vp_deficit_source",
+      "vp_source"
     )
   )
   expect_s3_class(wd, class = "data.table")
 })
+
+
+test_that("get_patched_point() returns selected daily values", {
+  vcr::use_cassette(
+    "silo_get_patched_point_selected_daily_values",
+    {
+      skip_if_offline()
+      expect_message(
+        wd <- get_patched_point(
+          station_code = "008137",
+          which_values = c("rain", "max_temp", "min_temp"),
+          start_date = "2021-06-01",
+          end_date = "2021-07-01",
+          api_key = Sys.getenv("SILO_API_KEY")
+        )
+      )
+  })
+  expect_equal(nrow(wd), 31)
+  expect_length(wd, 15)
+  expect_named(
+    wd,
+    c(
+      "station_code",
+      "station_name",
+      "longitude",
+      "latitude",
+      "elev_m",
+      "date",
+      "year",
+      "month",
+      "day",
+      "daily_rain",
+      "daily_rain_source",
+      "max_temp",
+      "max_temp_source",
+      "min_temp",
+      "min_temp_source"
+    )
+  )
+  expect_s3_class(wd, class = "data.table")
+})
+
