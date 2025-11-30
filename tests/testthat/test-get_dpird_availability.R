08c1edc9a00b4ade77f746167afca719119ae6cd
Adam H. Sparks <adamhsparks@icloud.com>
2025-11-30 11:26:45 +0800

    update vcr fixtures for tests

--- tests/testthat/test-get_dpird_availability.R
+++ tests/testthat/test-get_dpird_availability.R
@@ -35,65 +35,61 @@ test_that("get_dpird_availability() fails with bad user inputs", {
 })
 
 ## custom period availability ----
-test_that("get_dpird_availability() returns values",
-          {
-            vcr::use_cassette("dpird_period_availability", {
-              skip_if_offline()
-              x <- get_dpird_availability(
-                station_code = c("BD002", "SP"),
-                start_date = "20230401",
-                end_date = "20230402"
-              )
-            })
-            expect_s3_class(x, "data.table")
-            expect_identical(dim(x), c(2L, 6L))
-            expect_named(
-              x,
-              c(
-                "station_code",
-                "station_name",
-                "start_date",
-                "end_date",
-                "availability_since_9_am",
-                "availability_since_12_am"
-              )
-            )
-            expect_type(x$station_code, "integer")
-            expect_type(x$station_name, "character")
-            expect_s3_class(x$start_date, "POSIXct")
-            expect_s3_class(x$end_date, "POSIXct")
-          })
+test_that("get_dpird_availability() returns values", {
+  vcr::use_cassette("dpird_period_availability", {
+    skip_if_offline()
+    x <- get_dpird_availability(
+      station_code = c("BD002", "SP"),
+      start_date = "20230401",
+      end_date = "20230402"
+    )
+  })
+  expect_s3_class(x, "data.table")
+  expect_named(
+    x,
+    c(
+      "station_code",
+      "station_name",
+      "start_date",
+      "end_date",
+      "availability_since_9_am",
+      "availability_since_12_am"
+    )
+  )
+  expect_type(x$station_code, "integer")
+  expect_type(x$station_name, "character")
+  expect_s3_class(x$start_date, "POSIXct")
+  expect_s3_class(x$end_date, "POSIXct")
+})
 
 ## all station availability ----
-test_that("get_dpird_availability() returns values",
-          {
-            vcr::use_cassette("dpird_all_availability", {
-              skip_if_offline()
-              x <-
-                get_dpird_availability()
-            })
-            expect_s3_class(x, "data.table")
-            expect_identical(dim(x), c(238L, 15L))
-            expect_named(
-              x,
-              c(
-                "station_code",
-                "station_name",
-                "to9_am",
-                "since9_am",
-                "since12_am",
-                "current_hour",
-                "last24_hours",
-                "last7_days_since9_am",
-                "last7_days_since12_am",
-                "last14_days_since9_am",
-                "last14_days_since12_am",
-                "month_to_date_to9_am",
-                "month_to_date_since12_am",
-                "year_to_date_to9_am",
-                "year_to_date_since12_am"
-              )
-            )
-            expect_type(x$station_code, "integer")
-            expect_type(x$station_name, "character")
-          })
+test_that("get_dpird_availability() returns values", {
+  vcr::use_cassette("dpird_all_availability", {
+    skip_if_offline()
+    x <-
+      get_dpird_availability()
+  })
+  expect_s3_class(x, "data.table")
+  expect_named(
+    x,
+    c(
+      "station_code",
+      "station_name",
+      "to9_am",
+      "since9_am",
+      "since12_am",
+      "current_hour",
+      "last24_hours",
+      "last7_days_since9_am",
+      "last7_days_since12_am",
+      "last14_days_since9_am",
+      "last14_days_since12_am",
+      "month_to_date_to9_am",
+      "month_to_date_since12_am",
+      "year_to_date_to9_am",
+      "year_to_date_since12_am"
+    )
+  )
+  expect_type(x$station_code, "integer")
+  expect_type(x$station_name, "character")
+})
--- tests/testthat/test-get_stations_metadata.R
+++ tests/testthat/test-get_stations_metadata.R
@@ -1,221 +1,213 @@
+test_that("get_stations_metadata() functions properly for which_api = 'SILO'", {
+  vcr::use_cassette("metadata_silo_station", {
+    skip_if_offline()
+    x <-
+      get_stations_metadata(which_api = "silo", include_closed = TRUE)
+  })
+  expect_identical(dim(x), c(7995L, 11L))
+  expect_s3_class(x, "data.table")
+  expect_named(
+    x,
+    c(
+      "station_code",
+      "station_name",
+      "start",
+      "end",
+      "latitude",
+      "longitude",
+      "state",
+      "elev_m",
+      "source",
+      "status",
+      "wmo"
+    )
+  )
+})
 
-test_that("get_stations_metadata() functions properly for which_api = 'SILO'",
-          {
-            vcr::use_cassette("metadata_silo_station", {
-              skip_if_offline()
-              x <-
-                get_stations_metadata(which_api = "silo", include_closed = TRUE)
-            }
-            )
-            expect_identical(dim(x), c(7995L, 11L))
-            expect_s3_class(x, "data.table")
-            expect_named(
-              x,
-              c(
-                "station_code",
-                "station_name",
-                "start",
-                "end",
-                "latitude",
-                "longitude",
-                "state",
-                "elev_m",
-                "source",
-                "status",
-                "wmo"
-              )
-            )
-          })
-
-test_that("get_stations_metadata() functions properly for which_api = 'DPIRD'",
-          {
-            vcr::use_cassette("metadata_dpird_station", {
-              skip_if_offline()
-              x <-
-                get_stations_metadata(which_api = "dpird",
-                                     api_key = Sys.getenv("DPIRD_API_KEY"),
-                                     include_closed = TRUE,
-                                     rich = FALSE
-                                     )
-            }
-            )
-            expect_s3_class(x, "data.table")
-            expect_identical(dim(x), c(239L, 11L))
-            expect_named(
-              x,
-              c(
-                "station_code",
-                "station_name",
-                "start",
-                "end",
-                "latitude",
-                "longitude",
-                "state",
-                "elev_m",
-                "source",
-                "status",
-                "wmo"
-              )
-            )
-          })
+test_that("get_stations_metadata() functions properly for which_api = 'DPIRD'", {
+  vcr::use_cassette("metadata_dpird_station", {
+    skip_if_offline()
+    x <-
+      get_stations_metadata(
+        which_api = "dpird",
+        api_key = Sys.getenv("DPIRD_API_KEY"),
+        include_closed = TRUE,
+        rich = FALSE
+      )
+  })
+  expect_s3_class(x, "data.table")
+  expect_named(
+    x,
+    c(
+      "station_code",
+      "station_name",
+      "start",
+      "end",
+      "latitude",
+      "longitude",
+      "state",
+      "elev_m",
+      "source",
+      "status",
+      "wmo"
+    )
+  )
+})
 
 test_that("get_stations_metadata() functions properly for which_api = 'DPIRD'
-          w/ rich data",
-          {
-            vcr::use_cassette("metadata_dpird_station_rich_TRUE", {
-              skip_if_offline()
-              x <-
-                get_stations_metadata(
-                  which_api = "dpird",
-                  api_key = Sys.getenv("DPIRD_API_KEY"),
-                  include_closed = TRUE,
-                  rich = TRUE
-                )
-            })
-            expect_s3_class(x, "data.table")
-            expect_identical(dim(x), c(239L, 36L))
-            expect_named(
-              x,
-              c(
-                "station_code",
-                "station_name",
-                "start",
-                "end",
-                "latitude",
-                "longitude",
-                "state",
-                "elev_m",
-                "source",
-                "status",
-                "wmo",
-                "probe_height",
-                "rain_gauge_height",
-                "wind_probe_heights",
-                "air_temperature",
-                "battery_voltage",
-                "delta_t",
-                "dew_point",
-                "pan_evaporation",
-                "relative_humidity",
-                "barometric_pressure",
-                "rainfall",
-                "soil_temperature",
-                "solar_irradiance",
-                "wet_bulb",
-                "wind1",
-                "wind2",
-                "wind3",
-                "apparent_temperature",
-                "eto_short",
-                "eto_tall",
-                "frost_condition",
-                "heat_condition",
-                "wind_erosion_condition",
-                "richardson_unit",
-                "chill_hour"
-              )
-            )
-          })
+          w/ rich data", {
+  vcr::use_cassette("metadata_dpird_station_rich_TRUE", {
+    skip_if_offline()
+    x <-
+      get_stations_metadata(
+        which_api = "dpird",
+        api_key = Sys.getenv("DPIRD_API_KEY"),
+        include_closed = TRUE,
+        rich = TRUE
+      )
+  })
+  expect_s3_class(x, "data.table")
+  expect_named(
+    x,
+    c(
+      "station_code",
+      "station_name",
+      "start",
+      "end",
+      "latitude",
+      "longitude",
+      "state",
+      "elev_m",
+      "source",
+      "status",
+      "wmo",
+      "probe_height",
+      "rain_gauge_height",
+      "wind_probe_heights",
+      "air_temperature",
+      "battery_voltage",
+      "delta_t",
+      "dew_point",
+      "pan_evaporation",
+      "relative_humidity",
+      "barometric_pressure",
+      "rainfall",
+      "soil_temperature",
+      "solar_irradiance",
+      "wet_bulb",
+      "wind1",
+      "wind2",
+      "wind3",
+      "apparent_temperature",
+      "eto_short",
+      "eto_tall",
+      "frost_condition",
+      "heat_condition",
+      "wind_erosion_condition",
+      "richardson_unit",
+      "chill_hour"
+    )
+  )
+})
 
-test_that("get_station_metata() functions properly for which_api = 'all'",
-          {
-            vcr::use_cassette("metadata_all_cassette", {
-              skip_if_offline()
-              x <-
-                get_stations_metadata(which_api = "all",
-                                     api_key = Sys.getenv("DPIRD_API_KEY"))
-            })
+test_that("get_station_metata() functions properly for which_api = 'all'", {
+  vcr::use_cassette("metadata_all_cassette", {
+    skip_if_offline()
+    x <-
+      get_stations_metadata(
+        which_api = "all",
+        api_key = Sys.getenv("DPIRD_API_KEY")
+      )
+  })
 
-            expect_s3_class(x, "data.table")
-            expect_equal(ncol(x), 11L)
-            expect_true(nrow(x) >= 3800L && nrow(x) <= 3900L)  # Allow some flexibility for station changes
-            expect_named(
-              x,
-              c(
-                "station_code",
-                "station_name",
-                "start",
-                "end",
-                "latitude",
-                "longitude",
-                "state",
-                "elev_m",
-                "source",
-                "status",
-                "wmo"
-              )
-            )
-          })
+  expect_s3_class(x, "data.table")
+  expect_identical(ncol(x), 11L)
+  expect_true(nrow(x) >= 3800L && nrow(x) <= 3900L) # Allow some flexibility for station changes
+  expect_named(
+    x,
+    c(
+      "station_code",
+      "station_name",
+      "start",
+      "end",
+      "latitude",
+      "longitude",
+      "state",
+      "elev_m",
+      "source",
+      "status",
+      "wmo"
+    )
+  )
+})
 
-test_that("get_stations_metadata() fuzzy matches station names",
-          {
-            vcr::use_cassette("metadata_all_cassette", {
-              skip_if_offline()
-              x <-
-                get_stations_metadata(api_key = Sys.getenv("DPIRD_API_KEY"),
-                                     station_name = "Brisbane")
-            })
+test_that("get_stations_metadata() fuzzy matches station names", {
+  vcr::use_cassette("metadata_all_cassette", {
+    skip_if_offline()
+    x <-
+      get_stations_metadata(
+        api_key = Sys.getenv("DPIRD_API_KEY"),
+        station_name = "Brisbane"
+      )
+  })
 
-            expect_s3_class(x, "data.table")
-            expect_identical(dim(x), c(3L, 11L))
-            expect_named(
-              x,
-              c(
-                "station_code",
-                "station_name",
-                "start",
-                "end",
-                "latitude",
-                "longitude",
-                "state",
-                "elev_m",
-                "source",
-                "status",
-                "wmo"
-              )
-            )
-            expect_identical(x$station_name,
-                         c("Brisbane", "Brisbane Aero", "Mt Brisbane"))
-          })
+  expect_s3_class(x, "data.table")
+  expect_identical(dim(x), c(3L, 11L))
+  expect_named(
+    x,
+    c(
+      "station_code",
+      "station_name",
+      "start",
+      "end",
+      "latitude",
+      "longitude",
+      "state",
+      "elev_m",
+      "source",
+      "status",
+      "wmo"
+    )
+  )
+  expect_identical(
+    x$station_name,
+    c("Brisbane", "Brisbane Aero", "Mt Brisbane")
+  )
+})
 
-test_that("get_stations_metadata() matches station_codes",
-            {
-              vcr::use_cassette("metadata_all_cassette", {
-                skip_if_offline()
-                x <-
-                  get_stations_metadata(
-                    api_key = Sys.getenv("DPIRD_API_KEY"),
-                    station_code = c("YU001", "YU002", "YU003")
-                  )
-              })
+test_that("get_stations_metadata() matches station_codes", {
+  vcr::use_cassette("metadata_all_cassette", {
+    skip_if_offline()
+    x <-
+      get_stations_metadata(
+        api_key = Sys.getenv("DPIRD_API_KEY"),
+        station_code = c("YU001", "YU002", "YU003")
+      )
+  })
 
-            expect_s3_class(x, "data.table")
-            expect_identical(dim(x), c(3L, 11L))
-            expect_named(
-              x,
-              c(
-                "station_code",
-                "station_name",
-                "start",
-                "end",
-                "latitude",
-                "longitude",
-                "state",
-                "elev_m",
-                "source",
-                "status",
-                "wmo"
-              )
-            )
-            expect_identical(as.character(x$station_code),
-                         c("YU001", "YU002", "YU003"))
-          })
+  expect_s3_class(x, "data.table")
+  expect_identical(dim(x), c(3L, 11L))
+  expect_named(
+    x,
+    c(
+      "station_code",
+      "station_name",
+      "start",
+      "end",
+      "latitude",
+      "longitude",
+      "state",
+      "elev_m",
+      "source",
+      "status",
+      "wmo"
+    )
+  )
+  expect_identical(as.character(x$station_code), c("YU001", "YU002", "YU003"))
+})
 
 # no api_key
-test_that("get_stations_metadata() errors if no api_key is provide for DPIRD",
-          {
-            expect_error(get_stations_metadata(which_api = "dpird",
-                                               api_key = ""))
-            expect_error(get_stations_metadata(which_api = "all",
-                                               api_key = ""))
-          })
+test_that("get_stations_metadata() errors if no api_key is provide for DPIRD", {
+  expect_error(get_stations_metadata(which_api = "dpird", api_key = ""))
+  expect_error(get_stations_metadata(which_api = "all", api_key = ""))
+})
